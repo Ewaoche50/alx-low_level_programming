04f0cf0c0fe48f0c7094ea8a24b45d6e490be284
Ewaoche50 <ewaochedanny50@gmail.com>
2022-10-15 04:12:55 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"with proper grammar");
+	puts("\"but the outcome is a piece of art");
+	return (0);
+}
