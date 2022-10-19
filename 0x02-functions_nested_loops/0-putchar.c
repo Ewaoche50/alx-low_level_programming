2496ea5837bf351f1bd2bd62dde4a73d1b202654
Ewaoche50 <ewaochedanny50@gmail.com>
2022-10-19 06:38:14 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Main - print _putchar and a new line
- * Return: Always 0
- */
+* Main - program that prints _putchar 
+* Return: Always 0
+*/
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
