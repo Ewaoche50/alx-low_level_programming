5e7427d0a45fd37b22f3316c3fa944ab09c7e138
Ewaoche50 <ewaochedanny50@gmail.com>
2022-10-21 16:53:13 -0700

    supper 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _supper - checks if a character is uppercase
+ * @c: Variable text
+ * Return: Alwayss 0
+ */
+int _supper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
