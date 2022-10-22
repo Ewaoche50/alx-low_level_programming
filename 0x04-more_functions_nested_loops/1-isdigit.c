b16fe65be49cd5bede52930ddc0211d4d5dc3404
Ewaoche50 <113727117+Ewaoche50@users.noreply.github.com>
2022-10-22 01:20:54 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _indigit - checks if a char is a digit
+* @c: int to be checked
+* Return : if c is a digit, 0 otherwise 
+*/
+int _isdigit(int c)
+{
+  int y = 0;
+  
+  if (c >= '0'&& c <= '9')
+    y = 1;
+  
+  Return (y);
+}
