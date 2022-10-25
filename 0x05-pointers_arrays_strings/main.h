6f08663a205a56c1c0676149c0213f97a42e4b6c
Ewaoche50 <113727117+Ewaoche50@users.noreply.github.com>
2022-10-25 03:00:49 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
-#ifndef MATH_H
+#ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,6 +15,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c)
 
 #endif
+
